3f21146aceb509999695d7ae22f275010a399a24
nop33 <hliastrich@yahoo.gr>
2014-01-12 14:39:38 +0100

    Initial commit

---
+++ src/main/java/gsalgos/App.java
@@ -0,0 +1,10 @@
+package gsalgos;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello, world!");
+    }
+    
+}
---
+++ src/main/java/org/graphstream/algorithm/community/CommunityDetectionLouvain.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.graphstream.algorithm.community;
+
+/**
+ *
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public class CommunityDetectionLouvain {
+    
+}
