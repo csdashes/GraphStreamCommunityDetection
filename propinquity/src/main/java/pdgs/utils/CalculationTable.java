b9fbcd5a831eaf22d25347045c308a22c395de5f
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-04-15 16:54:08 +0200

    Delete garbage

--- propinquity/src/main/java/pdgs/utils/CalculationTable.java
+++ propinquity/src/main/java/pdgs/utils/CalculationTable.java
@@ -7,7 +7,6 @@
 package pdgs.utils;
 
 import com.google.common.collect.Sets;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -17,27 +16,27 @@ import java.util.Set;
 public class CalculationTable {
     public static Set<Integer> CalculateCrr(Set<Integer> Nr, Set<Integer> nnNr) {
         
-        return Sets.intersection(Nr, nnNr); //.copyInto(new LinkedHashSet<Integer>(20));
+        return Sets.intersection(Nr, nnNr);
     }
 
     public static Set<Integer> CalculateCri(Set<Integer> Nr, Set<Integer> Ni, Set<Integer> nnNr, Set<Integer> nnNi) {
         // (Nr ^ nnNi) + (Ni ^ nnNr) + (Ni ^ nnNi)
-        return Sets.union(Sets.union(Sets.intersection(Nr, nnNi), Sets.intersection(Ni, nnNr)), Sets.intersection(Ni, nnNi)); //.copyInto(new LinkedHashSet<Integer>(20));
+        return Sets.union(Sets.union(Sets.intersection(Nr, nnNi), Sets.intersection(Ni, nnNr)), Sets.intersection(Ni, nnNi));
     }
 
     public static Set<Integer> CalculateCrd(Set<Integer> Nr, Set<Integer> Nd, Set<Integer> nnNr, Set<Integer> nnNd) {
         // (Nr ^ nnNd) + (Nd ^ nnNr) + (Nd ^ nnNd)
-        return Sets.union(Sets.union(Sets.intersection(Nr, nnNd), Sets.intersection(Nd, nnNr)), Sets.intersection(Nd, nnNd)); //.copyInto(new LinkedHashSet<Integer>(20));
+        return Sets.union(Sets.union(Sets.intersection(Nr, nnNd), Sets.intersection(Nd, nnNr)), Sets.intersection(Nd, nnNd));
     }
 
     public static Set<Integer> CalculateCii(Set<Integer> Nr, Set<Integer> Ni, Set<Integer> nnNr, Set<Integer> nnNi) {
         // (Nr + Ni) ^ (nnNr + nnNi)
-        return Sets.intersection(Sets.union(Nr, Ni), Sets.union(nnNr, nnNi)); //.copyInto(new LinkedHashSet<Integer>(20));
+        return Sets.intersection(Sets.union(Nr, Ni), Sets.union(nnNr, nnNi));
     }
 
     public static Set<Integer> CalculateCdd(Set<Integer> Nr, Set<Integer> Nd, Set<Integer> nnNr, Set<Integer> nnNd) {
         // (Nr + Nd) ^ (nnNr + nnNd)
-        return Sets.intersection(Sets.union(Nr, Nd), Sets.union(nnNr, nnNd)); //.copyInto(new LinkedHashSet<Integer>(20));
+        return Sets.intersection(Sets.union(Nr, Nd), Sets.union(nnNr, nnNd));
     }
 
 }
