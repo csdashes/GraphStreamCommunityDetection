2f56a382f94c74354692fb22b72397b93e16ab1e
nop33 <hliastrich@yahoo.gr>
2014-05-16 23:38:02 +0200

    Enable louvain calculation with weight

--- propinquity/src/main/java/pdgs/propinquitydynamics/App.java
+++ propinquity/src/main/java/pdgs/propinquitydynamics/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.GraphParseException;
-//import pdgs.utils.Utils;
+import pdgs.utils.Utils;
 
 public class App {
     public static void main(String[] args) throws IOException, GraphParseException {
@@ -25,6 +25,6 @@ public class App {
         }
                 
         pd.getResults();
-//        Utils.exportGraphIntoGML(graph, "../data/export");
+        Utils.exportGraphIntoGML(graph, "../data/export");
     }
 }
