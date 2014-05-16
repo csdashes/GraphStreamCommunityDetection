5c70dfb224d71df49bbd98980aee989a5d781f78
nop33 <hliastrich@yahoo.gr>
2014-05-16 19:47:20 +0200

    Utils class, exportGraphToGML

---
+++ propinquity/src/main/java/pdgs/utils/Utils.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdgs.utils;
+
+import java.io.IOException;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.stream.file.FileSinkGML;
+
+/**
+ *
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public class Utils {
+    
+    public static void exportGraphIntoGML(Graph graph, String fileName) throws IOException {
+        for (Node n : graph.getEachNode()) {
+            n.addAttribute("ui_label", n.getAttribute("ui.label"));
+            n.addAttribute("ui_style", n.getAttribute("ui.style"));
+            n.clearAttributes();
+        }
+        for (Edge e : graph.getEachEdge()) {
+            e.addAttribute("weight", e.getAttribute("ui.label"));
+            e.addAttribute("ui_label", e.getAttribute("ui.label"));
+            e.addAttribute("ui_style", e.getAttribute("ui.style"));
+            e.clearAttributes();
+        }
+
+        FileSinkGML gml = new FileSinkGML();
+        gml.writeAll(graph, fileName + ".gml");
+    }
+}
