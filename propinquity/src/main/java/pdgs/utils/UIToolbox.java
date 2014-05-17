d65c625bb0e09ca97ccce6f67d5b56f07affaa57
nop33 <hliastrich@yahoo.gr>
2014-05-17 22:07:15 +0200

    Introduce UIToolbox class

---
+++ propinquity/src/main/java/pdgs/utils/UIToolbox.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdgs.utils;
+
+/**
+ *
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public class UIToolbox {
+    
+}
