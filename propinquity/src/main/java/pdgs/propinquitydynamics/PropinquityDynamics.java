bb9c2844d26f59a458909bf5e111e27001a84416
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-05-17 13:42:00 +0200

    Init e to -1, so we can ender first loop

--- propinquity/src/main/java/pdgs/propinquitydynamics/PropinquityDynamics.java
+++ propinquity/src/main/java/pdgs/propinquitydynamics/PropinquityDynamics.java
@@ -26,7 +26,7 @@ import pdgs.utils.PropinquityMap;
 public class PropinquityDynamics implements Algorithm {
 
     Graph graph;
-    private int a, b, e;
+    private int a, b, e = -1;
     private boolean debug = false, statistics = false;
     private String[] debugIDs;
 
