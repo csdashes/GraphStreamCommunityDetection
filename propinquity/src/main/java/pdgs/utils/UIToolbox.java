c61c99d225b170bf5cf4de7f7da4495be8f492db
nop33 <hliastrich@yahoo.gr>
2014-05-17 22:23:01 +0200

    Add and remove Sprite methods

--- propinquity/src/main/java/pdgs/utils/UIToolbox.java
+++ propinquity/src/main/java/pdgs/utils/UIToolbox.java
@@ -6,6 +6,8 @@ package pdgs.utils;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.ui.graphicGraph.stylesheet.StyleConstants;
+import org.graphstream.ui.spriteManager.Sprite;
 import org.graphstream.ui.spriteManager.SpriteManager;
 
 /**
@@ -27,5 +29,28 @@ public class UIToolbox {
     public static void styleNode(Node n) {
         n.setAttribute("ui.label", n.getIndex());
         n.setAttribute("ui.style", "size:20px;");
+    }
+    
+    /**
+     * Add a sprite in the graph Viewer window. Default X coordinate: 20.
+     * @param spriteId the id for the specific Sprite.
+     * @param spriteName the text that will be displayed as name of the sprite.
+     * @param spriteValue the value (int) next to the name of the sprite.
+     * @param spritePosition the Y coordinate of the sprite. Default X position is 20. 
+     */
+    public void addSprite(String spriteId, String spriteName, int spriteValue, int spritePosition) {
+        Sprite spr = this.sm.addSprite(spriteId);
+        spr.setPosition(StyleConstants.Units.PX, 20, spritePosition, 0);
+        spr.setAttribute("ui.label",
+                String.format(spriteName+": %d", spriteValue));
+        spr.setAttribute("ui.style", "size: 0px; text-color: rgb(150,100,100); text-size: 20;");
+    }
+    
+    /**
+     * Remove a Sprite from the graph Viewer window.
+     * @param spriteId the id of the sprite.
+     */
+    public void removeSprite(String spriteId) {
+        this.sm.removeSprite(spriteId);
     }
 }
