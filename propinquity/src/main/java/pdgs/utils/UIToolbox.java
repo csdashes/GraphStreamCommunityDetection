bd28e7bc59eb305700ad480be86068bb92f5e5ec
nop33 <hliastrich@yahoo.gr>
2014-05-17 22:16:30 +0200

    Add constructor in UIToolbox to init SpriteManager

--- propinquity/src/main/java/pdgs/utils/UIToolbox.java
+++ propinquity/src/main/java/pdgs/utils/UIToolbox.java
@@ -4,13 +4,21 @@
  */
 package pdgs.utils;
 
+import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.ui.spriteManager.SpriteManager;
 
 /**
  *
  * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
  */
 public class UIToolbox {
+    
+    private SpriteManager sm;
+    
+    public UIToolbox(Graph graph) {
+        this.sm = new SpriteManager(graph);
+    }
     
     /**
      * Method to add styling attributes (ui.label, ui.style) to a node
