666e83c0c28270841b1d095cd81fe0c15099621d
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-05-18 20:46:07 +0200

    In this class we will have extraction algorithms

---
+++ propinquity/src/main/java/pdgs/utils/ExtractCommunities.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pdgs.utils;
+
+/**
+ *
+ * @author Anastasis Andronidis <anastasis90@yahoo.gr>
+ */
+public class ExtractCommunities {
+    
+}
