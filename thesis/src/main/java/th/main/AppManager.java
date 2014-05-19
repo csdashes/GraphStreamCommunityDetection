1ed9511dc230530eb8f1854275e5be80bd95b122
nop33 <hliastrich@yahoo.gr>
2014-05-19 15:28:02 +0200

    Init printUserMenu method

--- thesis/src/main/java/th/main/AppManager.java
+++ thesis/src/main/java/th/main/AppManager.java
@@ -1,9 +1,32 @@
 package th.main;
 
+import th.utils.Menu;
+
 /**
  *
  * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
  */
 public class AppManager {
     
+    public void printUserMenu() {
+        int selection;
+        boolean flag = true;
+        
+        while(flag) {
+            selection = Menu.printMenu();
+            switch(selection) {
+                case 1:
+                    //Execute 1st function
+                    break;
+                case 2:
+                    //Execute 2st function
+                    break;
+                case 3:
+                    //Execute 3rd function
+                case 0:
+                    //Exit
+                    return;
+            }
+        }
+    }
 }
