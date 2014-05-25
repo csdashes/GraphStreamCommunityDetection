4a925b5812ddbb38921d59ca1db151d81b84bc38
nop33 <hliastrich@yahoo.gr>
2014-05-25 14:25:25 +0200

    Implement WeightMap functions
    Similar to PropinquityMap ones.

--- thesis/src/main/java/th/algorithms/louvain/utils/WeightMap.java
+++ thesis/src/main/java/th/algorithms/louvain/utils/WeightMap.java
@@ -8,4 +8,27 @@ import java.util.HashMap;
  */
 public class WeightMap extends HashMap<String, Double> {
 
+    public WeightMap(int cap) {
+        super(cap);
+    }
+    
+    public void increase(String communityId, Double weight) {
+        Double currentWeight = this.get(communityId);
+        if (currentWeight == null) {
+            this.put(communityId, weight);
+        } else {
+            currentWeight += weight;
+        }
+    }
+    
+    public void decrease(String communityId, Double weight) {
+        Double currentWeight = this.get(communityId);
+        if (currentWeight != null) {
+            currentWeight -= weight;
+        }
+    }
+    
+    public Double getWeight(String communityId) {
+        return this.get(communityId);
+    }
 }
