9cf27c32029cabc6a8c40a0ae2356e75351a336a
nop33 <hliastrich@yahoo.gr>
2014-05-25 14:15:46 +0200

    Introduce WeightMap

---
+++ thesis/src/main/java/th/algorithms/louvain/utils/WeightMap.java
@@ -0,0 +1,11 @@
+package th.algorithms.louvain.utils;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public class WeightMap extends HashMap<String, Double> {
+
+}
