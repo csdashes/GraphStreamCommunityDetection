da9af3e0835575b7b4b4743c72fb6f607762e9d0
nop33 <hliastrich@yahoo.gr>
2014-05-25 11:24:29 +0200

    Start over Louvain. Init node and community lists.

---
+++ thesis/src/main/java/th/algorithms/louvain/CommunityDetectionLouvain2.java
@@ -0,0 +1,211 @@
+package th.algorithms.louvain;
+
+import th.algorithms.louvain.utils.HyperCommunityManager;
+import th.algorithms.louvain.utils.HyperCommunity;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Set;
+import org.graphstream.algorithm.measure.Modularity;
+import org.graphstream.algorithm.measure.NormalizedMutualInformation;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.GraphParseException;
+import org.graphstream.ui.graphicGraph.stylesheet.StyleConstants.Units;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
+
+/**
+ * Implementation of the Louvain algorithm.
+ *
+ * @reference Fast unfolding of communities in large networks Vincent D.
+ * Blondel, Jean-Loup Guillaume, Renaud Lambiotte and Etienne Lefebvre.
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public class CommunityDetectionLouvain2 {
+
+    private Graph graph, // Graph used for the calculations
+            finalGraph;  // The final graph printed
+    private HyperCommunityManager manager;
+    private List<Map<String, HyperCommunity>> communitiesPerPhase; // Every item contains
+    // a map between community id and community object
+
+    private Modularity modularity;
+    private double maxModularity,
+            newModularity,
+            initialModularity,
+            deltaQ,
+            globalMaxQ,
+            globalNewQ;
+    private String oldCommunity,
+            bestCommunity;
+    // Used for colors.
+    private Random color;
+    private int r, g, b;
+
+    private Iterator<Node> neighbours;
+    private int step;
+    private String fileName;
+
+    // Sprites used to display the results on the screen.
+    private SpriteManager sm;
+    private Sprite communitiesCount,
+            modularityCount,
+            nmiCount;
+
+    private NormalizedMutualInformation nmi;
+    
+    private boolean debug = false;
+
+    /**
+     * Initializing global variables.
+     *
+     * @param fileName the input path of the file.
+     * @throws IOException
+     * @throws GraphParseException
+     */
+    public void init(String fileName) throws IOException, GraphParseException {
+
+        this.graph = new SingleGraph("communities");
+        this.graph.read(fileName); // Import from the text file.
+        //this.graph.display();
+
+        // Add weight to each edge
+        for (Edge edge : this.graph.getEdgeSet()) {
+            if (edge.hasAttribute("weight")) {
+                Double tmp = Double.parseDouble((String) edge.getAttribute("weight"));
+                edge.changeAttribute("weight", tmp);
+            } else {
+                edge.addAttribute("weight", 1.0);
+            }
+            edge.addAttribute("ui.label", edge.getAttribute("weight"));
+        }
+
+        // Add attribute "trueCommunityNodes" to every node, because later each
+        // node will represent a community (after the folding phase) so we want 
+        // to keep track of the contents of each community. Used to revert to
+        // the original graph.        
+        for (Node node : this.graph) {
+            Set<Integer> trueCommunityNodes = new HashSet<Integer>();
+            trueCommunityNodes.add(node.getIndex());
+            node.addAttribute("trueCommunityNodes", trueCommunityNodes);
+        }
+
+        this.communitiesPerPhase = new ArrayList<Map<String, HyperCommunity>>();
+        this.manager = new HyperCommunityManager();
+
+        this.globalMaxQ = -0.5; // making sure to have the lowest value
+        this.fileName = fileName;
+    }
+
+    /**
+     * The controller of the algorithm.
+     *
+     * @throws IOException
+     * @throws GraphParseException
+     */
+    public void execute() throws IOException, GraphParseException {
+
+        this.globalNewQ = findCommunities(); // Calculate the modularity after the first phase.
+//        finalGraph = this.graph;                  // Keep history of the first graph created.
+//        while (this.globalNewQ > this.globalMaxQ) {         // As long as the modularity is not the maximum
+//            this.globalMaxQ = this.globalNewQ;
+//            this.graph = foldingCommunities(this.graph); // go to the second phase (folding)
+//            globalNewQ = findCommunities();    // and get the new modularity
+//        }
+//        printFinalGraph(this.graph); // After reaching the maximum modularity, 
+        // print the graph on the screen.
+    }
+
+    public double findCommunities() {
+
+        // Mapping between community id and community object
+        Map<String, HyperCommunity> communities = new HashMap<String, HyperCommunity>();
+
+        for (Node node : this.graph) {
+            node.addAttribute("ui.label", node.getIndex()); // Add a label in every node
+            // with the index id of the node.
+            // Every node belongs to a different community.
+            HyperCommunity community = manager.communityFactory();
+
+            // Add community attribute to each node, so modularity alg can identify 
+            //which nodes belong to each community.
+            node.addAttribute("community", community.getId());
+
+            // Add the newly created community to the map
+            communities.put(community.getId(), community);
+
+            // This will keep track of the summary of the edge weights between each node
+            // to each community.
+            node.addAttribute("nodeToCommunityEdgesWeights", new HashMap<String, Double>());
+        }
+
+        // Add the new map of communities in an arraylist so the communities will
+        // not be mixed through the recursive steps of the algorithm.
+        this.communitiesPerPhase.add(communities);
+
+        for (Node node : this.graph) {
+            String nodeCommunityId = (String) node.getAttribute("community");
+            HyperCommunity nodeCommunity = communities.get(nodeCommunityId);
+            HashMap<String, Double> nodeToCommunityEdgesWeights = (HashMap<String, Double>) node.getAttribute("nodeToCommunityEdgesWeights");
+
+            neighbours = node.getNeighborNodeIterator();
+            while (neighbours.hasNext()) {
+
+                Node neighbour = neighbours.next();
+                String neighbourCommunityId = (String) neighbour.getAttribute("community");
+                
+                Edge edgeBetween = node.getEdgeBetween(neighbour);
+                Double edgeBetweenWeight = (Double) edgeBetween.getAttribute("weight");
+                
+                nodeCommunity.increaseEdgeWeightToCommunity(neighbourCommunityId, edgeBetweenWeight);
+                
+                nodeToCommunityEdgesWeights.put(neighbourCommunityId, edgeBetweenWeight);
+            }
+            
+            if(this.debug) {
+                System.out.println("Community " + nodeCommunityId + ": " + nodeToCommunityEdgesWeights);
+                System.out.println("Node " + node.getIndex() + ": " + nodeToCommunityEdgesWeights);
+            }
+        }
+
+        return 0;
+    }
+
+//    private void initializeWeightCountersToCommunities() {
+//
+//    }
+//
+//    public Graph foldingCommunities(Graph graph) {
+//
+//    }
+//
+//    public void printFinalGraph(Graph graph) {
+//
+//    }
+
+    /**
+     * Delaying a thread for 100ms. Used when displaying the communities on the
+     * screen.
+     */
+    protected void sleep() {
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+        }
+    }
+    
+    public void debugOn() {
+        this.debug = true;
+    }
+}
