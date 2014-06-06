ecc3c524914340e563ca72f5384ce06312fbedb0
nop33 <hliastrich@yahoo.gr>
2014-06-06 20:16:17 +0300

    Comment out code in main

--- thesis/src/main/java/th/main/Main.java
+++ thesis/src/main/java/th/main/Main.java
@@ -1,8 +1,17 @@
 package th.main;
 
+import com.google.common.collect.Sets;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.GraphParseException;
 import org.graphstream.util.parser.ParseException;
+import th.algorithms.propinquitydynamics.PropinquityDynamics;
+import th.algorithms.propinquitydynamics.utils.PropinquityMap;
 
 public class Main {
 
@@ -12,5 +21,31 @@ public class Main {
         AppManager appmana = new AppManager();
         appmana.printUserMenu();
         System.exit(0);
+        
+//        Graph graph = new DefaultGraph("Propinquity Dynamics");
+//        graph.display();
+//        graph.read("../data/karate.gml");
+//
+//        PropinquityDynamics pd = new PropinquityDynamics();
+//
+//        pd.init(graph);
+//        
+//        for (Edge edge : graph.getEachEdge()) {
+//            Node[] nodes = {edge.getNode0(), edge.getNode1()};
+//
+//            int maxNumEdges = 0;
+//            for (Node node : nodes) {
+//                if (node.getEdgeSet().size() > maxNumEdges) {
+//                    maxNumEdges = node.getEdgeSet().size();
+//                }
+//            }
+//
+//            // get the propinquity
+//            int prop = ((PropinquityMap) nodes[0].getAttribute("pm")).get(nodes[1].getIndex()).get();
+//
+//            edge.setAttribute("ui.label", prop);
+//            edge.setAttribute("ui.style", "text-color:red;text-style:bold; text-size:12;size:" + prop  + ";");
+//            edge.setAttribute("weight", prop);
+//        }
     }
 }
