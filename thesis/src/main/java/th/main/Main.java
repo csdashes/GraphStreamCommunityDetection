e349709f576f64302ed8c233054df5d5bce157a9
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-06-29 18:00:11 +0300

    exception

--- thesis/src/main/java/th/main/Main.java
+++ thesis/src/main/java/th/main/Main.java
@@ -12,7 +12,7 @@ import org.graphstream.util.parser.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, GraphParseException, ParseException {
+    public static void main(String[] args) throws IOException, GraphParseException, ParseException, InterruptedException {
         // We must create a menu. I am too lazy right now to do it -.-
         // I've got your back bro!
         AppManager appmana = new AppManager();
